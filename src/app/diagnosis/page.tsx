2825cd7dc2f04b54db62b54d33e4362574f50173
kennethkvs <kennethkvs060103@gmail.com>
2024-05-17 11:42:56 -0400

    Add Pages

---
+++ src/app/diagnosis/page.tsx
@@ -0,0 +1,7 @@
+export default function DiagnosisPage() {
+  return (
+    <div>
+      <h1>Diagnosis</h1>
+    </div>
+  );
+}
---
+++ src/app/get-details/page.tsx
@@ -0,0 +1,7 @@
+export default function GetDetailsPage() {
+  return (
+    <div>
+      <h1>Get Patient Details</h1>
+    </div>
+  );
+}
